7d6104a7ceffb7fa05afc9e6e1ec2bb1161dd311
Tanya Krishnakumar <tanyakris@gmail.com>
2015-02-19 01:36:16 -0800

    Hi

---
+++ js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+    //initialise Stellar.js
+    $(window).stellar();
+    //Cache some variables
+    var links = $('.nav').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+    //Setup waypoints plugin
+ 
+
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
+    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 1500, 'easeInOutQuint');
+    }
+    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+});
